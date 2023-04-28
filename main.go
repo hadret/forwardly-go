b2f7194473a67619dc867e3fed6030936cc499d3
Filip Chabik <filip@ghost.org>
2023-04-28 12:48:43 +0200

    Switched to viper for environment variables handling

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golobby/dotenv"
-	"golang.org/x/exp/slices"
+	"github.com/spf13/viper"
 )
 
 type Alert struct {
@@ -16,29 +14,18 @@ type Alert struct {
 	Status   string `form:"status" json:"status" binding:"required"`
 }
 
-type Config struct {
-	Env_Name    string   `env:"ENV_NAME"`
-	Kuma_Url    string   `env:"KUMA_URL"`
-	Kuma_Tokens []string `env:"KUMA_TOKENS"`
-}
-
 type URLToken struct {
 	ID string `uri:"id" binding:"required"`
 }
 
 func main() {
-	// Load environment variables from file or fallback to global ones
-	config := Config{}
-	file, err := os.Open(".env")
-	if err != nil {
-		config.Env_Name = os.Getenv("ENV_NAME")
-		config.Kuma_Url = os.Getenv("KUMA_URL")
-		config.Kuma_Tokens = strings.Split(os.Getenv("KUMA_TOKENS"), ",")
-	}
-	err = dotenv.NewDecoder(file).Decode(&config)
+	// Load environment variables
+	viper.AutomaticEnv()
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
 
 	// Set to release mode when in production environment
-	if config.Env_Name == "Production" {
+	if viper.Get("ENV_NAME") == "Production" {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
@@ -46,7 +33,7 @@ func main() {
 
 	// Respond with simple pong
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(200, "Pong")
 	})
 
 	// Redirect to projects GH page
@@ -59,11 +46,13 @@ func main() {
 		// Assign environment variables and structs
 		var url_token URLToken
 		var alert Alert
+		kuma_tokens := viper.GetString("KUMA_TOKENS")
+		kuma_url := viper.GetString("KUMA_URL")
 
 		// Do the redirect or fail trying
 		if c.ShouldBindUri(&url_token) == nil && c.ShouldBind(&alert) == nil {
-			if slices.Contains(config.Kuma_Tokens, url_token.ID) {
-				resp, err := http.Get(fmt.Sprintf("%v/%v", config.Kuma_Url, url_token.ID))
+			if strings.Contains(kuma_tokens, url_token.ID) {
+				resp, err := http.Get(fmt.Sprintf("%v/%v", kuma_url, url_token.ID))
 				if err != nil {
 					fmt.Printf("Error when trying to reach Uptime Kuma: %v", err)
 					return
